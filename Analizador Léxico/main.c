a756733569cb4e68b224688befc0848673eed205
JavierP08 <a01662438q@tec.mx>
2025-04-24 14:16:30 -0600

    tabla de transiciones

--- Analizador Léxico/main.c
+++ Analizador Léxico/main.c
@@ -3,9 +3,9 @@
 #include <stdbool.h>
 
 #define NUM_STATES 3
-#define NUM_CHAR 8
+#define NUM_CHAR 8 
 
-int TT[NUM_STATES][NUM_CHAR];
+int TT[NUM_STATES][NUM_CHAR]; // Tabla de transiciones
 
 void StartTable(){
     TT[0]['letter'] = 1;
@@ -16,6 +16,7 @@ void StartTable(){
     TT[0]['('] = 5;
     TT[0][')'] = 6;
     TT[0]['blank'] = 0;
+    TT[0]['rare_char'] = 0;
 
     TT[1]['letter'] = 1;
     TT[1]['digit'] = 1;
@@ -25,18 +26,26 @@ void StartTable(){
     TT[1]['('] = 2;
     TT[1][')'] = 2;
     TT[1]['blank'] = 2;
+    TT[1]['rare_char'] = 7;
 }
 
 bool Acceptance(int state){
-    return true;
+    if(state >= 0 && state < 7){
+        return true;
+    }
+    return false;
 }
 
 bool Error(int state){
+    if(state == 7){
+        return true;
+    }
     return false;
 }
 
 bool Advance(int state, char currentCh){
-    return true;
+    if(state)
+    return false;
 }
 
 void recordToken(char currentCh){
@@ -53,7 +62,8 @@ int main(){
         int state = 0; /* start */
         
         while(!Acceptance(state) && !Error(state)){
-            state = 1;
+            state = TT[state][currentCh];
+
             if(Advance(state, currentCh)){
                 currentCh = fgetc(file);
             }
