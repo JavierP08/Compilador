5ab936cfb57e909165e31b7d24f2153c605ad6f5
JavierP08 <a01662438q@tec.mx>
2025-05-02 10:28:24 -0600

    Debugg de código

--- Analizador Léxico/main.c
+++ Analizador Léxico/main.c
@@ -1,36 +1,54 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
+#include <ctype.h>
 
-#define NUM_STATES 3
-#define NUM_CHAR 8 
+#define NUM_STATES 7
+#define NUM_CHAR 9
 
 int TT[NUM_STATES][NUM_CHAR]; // Tabla de transiciones
 
+char PR[15][10] = {
+    "class", "struct", "def", 
+    "void", "int", "float",
+    "double", "char", "bool",
+    "long", "string",
+}; //Tabla de palabras reservadas
+
 void StartTable(){
-    TT[0]['letter'] = 1;
-    TT[0]['digit'] = 7;
+    TT[0]['l'] = 1;
+    TT[0]['d'] = 7;
     TT[0]['_'] = 1;
     TT[0]['{'] = 3;
     TT[0]['}'] = 4;
     TT[0]['('] = 5;
     TT[0][')'] = 6;
-    TT[0]['blank'] = 0;
-    TT[0]['rare_char'] = 0;
+    TT[0]['b'] = 0;
 
-    TT[1]['letter'] = 1;
-    TT[1]['digit'] = 1;
+    TT[1]['l'] = 1;
+    TT[1]['d'] = 1;
     TT[1]['_'] = 1;
     TT[1]['{'] = 2;
     TT[1]['}'] = 2;
     TT[1]['('] = 2;
     TT[1][')'] = 2;
-    TT[1]['blank'] = 2;
-    TT[1]['rare_char'] = 7;
+    TT[1]['b'] = 2;
+}
+
+char classify(char currentCh){
+    if(isalpha(currentCh)){
+        return 'l';
+    } else if (isdigit(currentCh)) {
+        return 'd';
+    } else if(isspace(currentCh)) {
+        return 'b';
+    }
+
+    return currentCh;
 }
 
 bool Acceptance(int state){
-    if(state >= 0 && state < 7){
+    if(state > 1 && state < 7){
         return true;
     }
     return false;
@@ -44,35 +62,88 @@ bool Error(int state){
 }
 
 bool Advance(int state, char currentCh){
-    if(state)
-    return false;
+    if(state > 1 || state <= 7){
+        return false;
+    }
+    return true;
 }
 
-void recordToken(char currentCh){
-
+void recordToken(char palabra[200], FILE* tokenFile){
+    char token[20];
+    sprintf(token, "<%s>", palabra);
+    
+    fprintf(tokenFile, "%s\n", token);
 }
 
-int main(){
-    FILE *file;
-    file = fopen("example.cpp", "r");
+/*int main(){
+    //Se abre el archivo a analizar y crear archivo de tokens
+    FILE *processFile;
+    FILE *tokenFile;
+    processFile = fopen("example.txt", "r");
+    tokenFile = fopen("tokens.txt", "w");
 
-    char currentCh = fgetc(file);
+    //Se establece la tabla de estados
+    StartTable();
 
-    while(currentCh != EOF){
-        int state = 0; /* start */
-        
+    //Se obtiene el primer char del archivo
+    char currentCh = fgetc(processFile);
+
+    while(numero < 5){
+        int state = 0;  //start 
+        char palabra[200] = "";
+        int index = 0;
+
         while(!Acceptance(state) && !Error(state)){
-            state = TT[state][currentCh];
+            char ch = classify(currentCh);
+            state = TT[state][ch];
+            printf("Current state: %d", state);
 
             if(Advance(state, currentCh)){
-                currentCh = fgetc(file);
+                palabra[index++] = currentCh;
+                currentCh = fgetc(processFile);
             }
-        }
-        if(Acceptance(state)){
-            printf("%c\n", currentCh);
-            recordToken(currentCh);
-        }else{
-            printf("Error en token");
+
         }
     }
+}*/
+
+int main(){
+    FILE *processFile;
+    FILE *tokenFile;
+    processFile = fopen("example.txt", "r");
+    tokenFile = fopen("tokens.txt", "w");
+
+    StartTable();
+
+    char currentCh = fgetc(processFile);
+
+    int state = 0;  /* start */
+    char palabra[200] = "";
+    int index = 0;
+
+    while (currentCh != EOF)
+    {
+        if(currentCh == '\t'){
+            currentCh = fgetc(processFile);
+        }
+        printf("Current state: %d, Current char: %c, Current word: %s\n", state, currentCh, palabra);
+
+        char ch = classify(currentCh);
+        state = TT[state][ch];
+        palabra[index++] = currentCh;
+
+        if(Acceptance(state)){
+            palabra[index] = '\0';
+            recordToken(palabra, tokenFile);
+            palabra[0] = '\0';
+            index = 0;
+        }
+
+        currentCh = fgetc(processFile);
+    }
+
+    recordToken(palabra, tokenFile);
+
+    fclose(processFile);
+    fclose(tokenFile);
 }
