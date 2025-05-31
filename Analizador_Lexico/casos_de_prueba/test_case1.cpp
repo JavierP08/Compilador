3bdd49fba7f51c3ebcce9a1d694eb3883246951a
JavierP08 <a01662438q@tec.mx>
2025-05-31 14:26:08 -0600

    finalizar sintactico

--- Analizador_Lexico/casos_de_prueba/test_case1.cpp
+++ Analizador_Lexico/casos_de_prueba/test_case1.cpp
@@ -1,19 +1,2 @@
-class Animal ¿ {
-    int edad;
-    char genero;
-    void hacerSonido() {
-        #sonido ;
-    }
-};
-
-struct Coordenada {
-    double x;
-    double y;
-    void mover() {};
-};
-
-int main() {
-    class Perro {};
-    struct Gato {};
-    long tamano;
+class animal {
 };
--- Analizador_sintactico/main.c
+++ Analizador_sintactico/main.c
@@ -4,6 +4,21 @@
 #include <ctype.h>
 #include <string.h>
 
+bool programa();
+bool clases();
+bool clases_();
+bool funciones();
+bool funciones_();
+bool instrucciones();
+bool instrucciones_();
+bool contenido();
+bool variable();
+bool variable_();
+bool match(int expected);
+void limpiarToken(const char* line);
+void error_();
+
+
 char* TokenIds[] = {
     "class", "main", "{", "}", 
     "(", ")"
@@ -12,139 +27,222 @@ char* TokenIds[] = {
 bool banderaOOP = false;
 bool banderaPro = false;
 
-bool programa(int token){
+int totalTokens = 0;
+int currentTokens = 0;
+
+FILE* file;
+FILE* saveFile;
+
+int token = 0;
+
+bool programa(){
     if(token == 1){
-        bool class_production = clases(token);
-        bool programa_recursion = programa(token);
+        if(!clases(token)) return false;
+        banderaOOP = true;
+        if(!programa(token)) return false;
+        return true;
     }else if(token == 7){
-        bool funcion_production = funciones(token);
-        bool variable_production = variable(token);
-
-        bool programa_recursion = programa(token);
-    }else{
-        Error();
-    }
-}
-
-bool clases(int token){
-    if(token == 1){
-        bool variable_prodcuction = variable(token);
-        if(token != 3) return false;
-        bool funciones_production = funciones(token);
-        if(token != 4) return false;
-        bool clases_recursion = clases_(token);
-
+        if(funciones(token)){
+            if(!funciones(token)) return false;
+            banderaPro = true;
+            if(!programa(token)) return false;
+        }else if(variable(token)){
+            if(!variable(token)) return false;
+            if(!programa(token)) return false;
+        }
+        return true;
+    }else if(token == 0){
         return true;
     }else{
-        Error();
+     error_();
     }
 }
 
-bool clases_(int token){
+bool clases(){
     if(token == 1){
-        bool classes_production = clases(token);
+        if(!match(1)) return false;
+        if(!variable(token)) return false;
+        if(!match(3)) return false;
+        if(!funciones(token)) return false;
+        if(!match(4)) return false;
+        if(!clases_(token)) return false;
+        return true;
+    }else{
+     error_();
+    }
+}
+
+bool clases_(){
+    if(token == 1){
+        if(!clases(token)) return false;
     }else if(token == 7){
-        bool variable_production = variable(token);
-        if(token != 2) return false;
-        if(token != 5) return false;
-        if(token != 6) return false;
-        if(token != 3) return false;
-        bool instrucciones_production = instrucciones(token);
-        if(token != 4) return false;
-
+        if(!variable(token)) return false;
+        if(!match(2)) return false;
+        if(!match(5)) return false;
+        if(!match(6)) return false;
+        if(!match(3)) return false;
+        if(!instrucciones(token)) return false;
+        if(!match(4)) return false;
+        return true;
+    }else if(token == 4 || token == 0){
         return true;
     }else{
-        Error();
+     error_();
     }
 }
 
-bool funciones(int token){
+bool funciones(){
     if(token == 7){
-        bool variable_production = variable(token);
-        if(token != 5) return false;
-        bool variable_production2 = variable(token);
-        if(token != 6) return false;
-        if(token != 3) return false;
-        bool instrucion_production = instrucciones(token);
-        if(token != 4) return false;
-        bool funcion_recursive = funciones_(token);
-
+        if(!variable(token)) return false;
+        if(!match(5)) return false;
+        if(!variable(token)) return false;
+        if(!match(6)) return false;
+        if(!match(3)) return false;
+        if(!instrucciones(token)) return false;
+        if(!match(4)) return false;
+        if(!funciones_(token)) return false;
         return true;
     }else{
-        Error();
+     error_();
     }
 }
 
-bool funciones_(int token){
+bool funciones_(){
     if(token == 7){
-        bool funciones_production = funciones(token);
+        if(!funciones(token)) return false;
+        return true;
+    }else if(token == 1 || token == 4 || token == 0){
+        return true;
     }else{
-        Error();
+     error_();
     }
 }
 
-bool instrucciones(int token){
+bool instrucciones(){
     if(token == 7){
-        bool variable_production = variable(token);
-        if(token != 3) return false;
-        bool instruction_production = instrucciones_(token);
-        if(token != 4) return false;
-
-        bool function_production = funciones(token);
+        if(!variable(token)) return false;
+        if(!match(3)) return false;
+        if(!instrucciones_(token)) return false;
+        if(!match(4)) return false;
+        if(!funciones(token)) return false;
+        return true;
+    }else if(token == 1){
+        if(!contenido(token)) return false;
+        return true;
+    }else if(token == 4){
+        return true;
+    }else{
+     error_();
     }
 }
 
 bool instrucciones_(){
-    return false;
+    if(token == 3){
+        if(!instrucciones(token)) return false;
+        if(!match(4)) return false;
+        return true;
+    }else if(token == 4){
+        return true;
+    }else{
+     error_();
+    }
 }
 
 bool contenido(){
-    return false;
+    if(token == 1){
+        if(!clases(token)) return false;
+        if(!contenido(token)) return false;
+        return true;
+    }else if(token == 7){
+        if(!funciones(token)) return false;
+        if(!contenido(token)) return false;
+        return true;
+    }else if(token == 4){
+        return true;
+    }else{
+     error_();
+    }
 }
 
 bool variable(){
-    return false;
+    if(token == 7){
+        if(!match(7)) return false;
+        if(!variable_(token)) return false;
+        return true;
+    }else{
+     error_();
+    }
 }
 
 bool variable_(){
-    return false;
+    if(token == 7){
+        if(!variable(token)) return false;
+        return true;
+    }else if(token == 1 || token == 7 || token == 0 || token == 3 || token == 2 || token == 5 || token == 4 || token == 6){
+        return true;
+    }else{
+     error_();
+    }
 }
 
-void Error(){
 
+void error_(){
+    int porcentaje = (currentTokens / totalTokens) * 100;
 }
 
-void limpiarToken(const char* line, char* cleaned) {
+void limpiarToken(const char* line) {
     int j = 0;
+    char clean[256];
 
     for (int i = 0; line[i] != '\0'; i++) {
         if (line[i] == ',') {
             break;
         }
         if (line[i] != '<' && line[i] != '>') {
-            cleaned[j++] = line[i];
+            clean[j++] = line[i];
         }
     }
-
-    cleaned[j] = '\0';
+    clean[j] = '\0';
+    sscanf(clean, "%d", &token);
 }
 
-bool match(int num){
+bool match(int expected){
+    if(token == expected){
+        char line[256];
+        if(fgets(line, sizeof(line), file)){
+            limpiarToken(line);
+        }else{
+            token = 0;
+        }
+        return true;
+    }
 
+    return false;
 }
 
 int main() {
-    FILE *file;
     char line[256];
-    char token[256];
 
     file = fopen("./test_case3.txt", "r");
+    saveFile = fopen("SaveFile.txt", "w");
 
-    while (fgets(line, sizeof(line), file)) {
-        limpiarToken(line, token);
+    fgets(line, sizeof(line), file);
+    limpiarToken(line);
 
+    programa(token);
+
+    if(banderaOOP && banderaPro){
+        printf("Es hibrido\n");
+    }
+
+    if(banderaOOP){
+        printf("Es oop\n");
+    }else if(banderaPro){
+        printf("Es procedimental\n");
+    }else{
+        printf("Es texto\n");
     }
 
     fclose(file);
-    return 0;
+    fclose(saveFile);
 }
